d9c3cf715c65bfbb8561bd0b56fa406c7e08765a
Venu varma <konduruvenuvarma123@outlook.com>
2025-04-01 18:21:38 +0530

    Shop Owner page partially completed

--- frontend/src/Components/ShopOwnerPage/ShopOwnerPage.jsx
+++ frontend/src/Components/ShopOwnerPage/ShopOwnerPage.jsx
@@ -5,25 +5,25 @@ import BannerSlider from "./BannerSlider.jsx";
 import { HiArrowSmRight, HiArrowSmLeft } from "react-icons/hi";
 
 const ShopOwnerPage = () => {
-  const [shopDetails, setShopDetails] = useState([]);
-  const [activeSlide, setActiveSlide] = useState(0);
-  const [status, setStatus] = useState(false);
+    const [shopDetails, setShopDetails] = useState([]);
+    const [activeSlide, setActiveSlide] = useState(0);
+    const [status, setStatus] = useState(false);
 
-  const handleClick = () => {
-    setStatus(!status);
-  };
+    const handleClick = () => {
+        setStatus(!status);
+    };
 
-  const handleClickNext = () => {
-    if (activeSlide < shopDetails.length - 1) {
-      setActiveSlide(activeSlide + 1);
-    }
-  };
+    const handleClickNext = () => {
+        if (activeSlide < shopDetails.length - 1) {
+            setActiveSlide(activeSlide + 1);
+        }
+    };
 
-  const handleClickPrev = () => {
-    if (activeSlide > 0) {
-      setActiveSlide(activeSlide - 1);
-    }
-  };
+    const handleClickPrev = () => {
+        if (activeSlide > 0) {
+            setActiveSlide(activeSlide - 1);
+        }
+    };
 
     const getData = async () => {
         try {
@@ -31,7 +31,7 @@ const ShopOwnerPage = () => {
             const response = await fetch("http://127.0.0.1:8000/api/shop_create_view/", {
                 method: "GET",
                 headers: {
-                    "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzQzNTQ4NTg1LCJpYXQiOjE3NDM0Mjk3ODUsImp0aSI6IjUxOGE3ZTBiZDRhZDQyMzI4YTIyYWQxYjk2ZmU4ZDAwIiwidXNlcl9pZCI6MX0.7LrEZOpue_DSHjr_MV37z_k0HoEZU4V6B41KfmgT_7I",
+                    "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzQzNjIxNDAxLCJpYXQiOjE3NDM1MDI2MDEsImp0aSI6ImFmODIyYTBhM2RiMzQzNWQ4OTRjOGZhODJkZGQzODk4IiwidXNlcl9pZCI6M30.BDC7bWWdpkgKs1GAPGRJGTI_AnhcgtGPgRjCuhMIjMc",
                 },
             });
             const data = await response.json();
@@ -44,14 +44,13 @@ const ShopOwnerPage = () => {
         }
     };
 
-  useEffect(() => {
-    getData();
-  }, []);
-
+    useEffect(() => {
+        getData();
+    }, []);
     return (
         <div className='flex justify-center items-center mt-5'>
             <div className='p-5 border'>
-                <BannerSlider images={images} />
+                {shopDetails[activeSlide]?.images && <BannerSlider images={shopDetails[activeSlide].images} />}
                 {shopDetails.length > 0 && (
                     <>
                         <div className='flex justify-between items-center mt-5'>
